17026d75acfbf4463edd925a0ef154defe598df2
Eugene Kosinskiy <e.kosinskiy@svitla.com>
2021-05-06 08:31:06 +0300

    demo project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ContactsComponent} from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contact', component: ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
